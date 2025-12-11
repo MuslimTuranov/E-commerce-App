6df8999999c443b7da841abc6305f7fed8092816
Muslim Turanov <mtuanov@mail.ru>
2025-12-11 23:15:03 +0500

    major changes

--- product-service/src/main/resources/db/migration/V1__create_product_table.sql
+++ product-service/src/main/resources/db/migration/V1__create_product_table.sql
@@ -6,9 +6,8 @@ CREATE TABLE IF NOT EXISTS product (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price NUMERIC(19,2) NOT NULL,
-    quantity INTEGER DEFAULT 0
+    quantity INTEGER NOT NULL DEFAULT 0
     );
 
 CREATE INDEX IF NOT EXISTS idx_product_sku_code ON product(sku_code);
-
 CREATE INDEX IF NOT EXISTS idx_product_name ON product(name);
